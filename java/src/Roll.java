ec6cbe19ca44afe29ebfa2e9c3de6b4992881336
Smit6077 <61833934+Smit6077@users.noreply.github.com>
2021-03-22 20:04:12 +0100

    Update Roll.java

--- java/src/Roll.java
+++ java/src/Roll.java
@@ -10,18 +10,96 @@ public class Roll {
     }
 
     // Attributes
+    private int diceValue;
+    private int nbRoll;
+    private int modifier;
+    private boolean isPositive;
+    private boolean wrongMod = false;
+    private Dice dice;
+    
 
     public Roll(String formula) {
-        // TODO
+
+        
+
+        if (Integer.valueOf(formula.charAt(0)) >= 97) {
+            this.nbRoll = 1;
+            this.diceValue = Integer.valueOf(formula.charAt(1)) - 48;
+            this.modifier = Integer.valueOf(formula.charAt(3)) - 48;
+
+            if(Integer.valueOf(formula.charAt(2)) == 43) {
+                isPositive = true;
+            }else if(Integer.valueOf(formula.charAt(3)) == 45){
+                isPositive = false;
+            }else{
+                wrongMod = true;
+            }
+
+
+        }else {
+
+
+            this.nbRoll = Integer.valueOf(formula.charAt(0)) - 48;
+            this.diceValue = Integer.valueOf(formula.charAt(2)) - 48;
+            this.modifier = Integer.valueOf(formula.charAt(4)) - 48;
+
+            if(Integer.valueOf(formula.charAt(3)) == 43){
+                isPositive = true;
+                System.out.println("Bonjour");
+
+            }else if(Integer.valueOf(formula.charAt(3)) == 45){
+                isPositive = false;
+                System.out.println("Non");
+
+            }else{
+                wrongMod = true;
+            }
+
+        }
+
+
     }
 
+
     public Roll(int diceValue, int nbRoll, int modifier) {
-        // TODO
+
+        this.diceValue = diceValue;
+        this.nbRoll = nbRoll;
+        this.modifier = modifier;
+
+
+
     }
 
     public int makeRoll(RollType rollType) {
-        // TODO
-        return 0;
+
+        this.dice = new Dice(diceValue);
+        int result;
+
+        if(wrongMod== true){
+            return result = -1;
+
+        } else {
+
+            if (isPositive == true) {
+                result = nbRoll * this.dice.rollDice() + modifier;
+            } else {
+                result = nbRoll * this.dice.rollDice() - modifier;
+            }
+        }
+        
+
+        if (result <0){
+            result = 0;
+        }
+
+        if (nbRoll <=0 || diceValue <= 0 ){
+            result = -1;
+        }
+
+        return result;
+        
+
     }
 
 }
