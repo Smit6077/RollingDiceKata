a13ae8f4d1e446796c3a357f3851bdf1ac34f975
Smit6077 <61833934+Smit6077@users.noreply.github.com>
2021-03-23 14:44:50 +0100

    Update Roll.java

--- java/src/Roll.java
+++ java/src/Roll.java
@@ -14,50 +14,47 @@ public class Roll {
     private int nbRoll;
     private int modifier;
     private boolean isPositive;
-    private boolean wrongMod = false;
     private Dice dice;
-    
+    public int result;
+    public int x =0;
+
+    private String[] formula2;
 
     public Roll(String formula) {
 
-        
-
         if (Integer.valueOf(formula.charAt(0)) >= 97) {
-            this.nbRoll = 1;
-            this.diceValue = Integer.valueOf(formula.charAt(1)) - 48;
-            this.modifier = Integer.valueOf(formula.charAt(3)) - 48;
-
-            if(Integer.valueOf(formula.charAt(2)) == 43) {
-                isPositive = true;
-            }else if(Integer.valueOf(formula.charAt(3)) == 45){
-                isPositive = false;
-            }else{
-                wrongMod = true;
-            }
+                this.nbRoll = 1;
+                this.diceValue = Integer.valueOf(formula.charAt(1)) - 48;
+                this.modifier = Integer.valueOf(formula.charAt(3)) - 48;
 
 
-        }else {
+                if (Integer.valueOf(formula.charAt(2)) == 43) {
+                    isPositive = true;
+                } else if (Integer.valueOf(formula.charAt(2)) == 45) {
+                    isPositive = false;
+                }
 
 
-            this.nbRoll = Integer.valueOf(formula.charAt(0)) - 48;
-            this.diceValue = Integer.valueOf(formula.charAt(2)) - 48;
-            this.modifier = Integer.valueOf(formula.charAt(4)) - 48;
+        } else {
 
-            if(Integer.valueOf(formula.charAt(3)) == 43){
-                isPositive = true;
-                System.out.println("Bonjour");
 
-            }else if(Integer.valueOf(formula.charAt(3)) == 45){
-                isPositive = false;
-                System.out.println("Non");
+                this.nbRoll = Integer.valueOf(formula.charAt(0)) - 48;
+                this.diceValue = Integer.valueOf(formula.charAt(2)) - 48;
+                this.modifier = Integer.valueOf(formula.charAt(4)) - 48;
 
-            }else{
-                wrongMod = true;
-            }
+                if (Integer.valueOf(formula.charAt(3)) == 43) {
+                    isPositive = true;
+                    System.out.println("Bonjour");
+
+                } else if (Integer.valueOf(formula.charAt(3)) == 45) {
+                    isPositive = false;
+                    System.out.println("Non");
+
+                }
+
 
         }
-
-
+        x= 2;
     }
 
 
@@ -66,40 +63,46 @@ public class Roll {
         this.diceValue = diceValue;
         this.nbRoll = nbRoll;
         this.modifier = modifier;
-
-
+        x = 1;
 
     }
 
     public int makeRoll(RollType rollType) {
 
         this.dice = new Dice(diceValue);
-        int result;
 
-        if(wrongMod== true){
-            return result = -1;
-
-        } else {
+        if(x!=1) {
 
             if (isPositive == true) {
                 result = nbRoll * this.dice.rollDice() + modifier;
             } else {
                 result = nbRoll * this.dice.rollDice() - modifier;
             }
-        }
-        
 
-        if (result <0){
-            result = 0;
+        }else {
+
+            result = nbRoll * this.dice.rollDice() + modifier;
+
         }
 
-        if (nbRoll <=0 || diceValue <= 0 ){
-            result = -1;
-        }
+            System.out.println(nbRoll);
+            System.out.println(modifier);
+            System.out.println(this.dice.rollDice());
+
+            System.out.println(result);
+
+            if (result < 0) {
+                result = 0;
+            }
+
+            if (nbRoll <= 0 || diceValue <= 0) {
+                result = -1;
+            }
 
         return result;
-        
+
 
     }
 
 }
+
